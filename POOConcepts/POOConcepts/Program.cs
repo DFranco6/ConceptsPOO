ea445f5757a8a8432e496fe2c49c5f52ad9ca47a
DANY.FRANCO <DANY.FRANCO6424@CORREO.POLICIA.GOV.CO>
2022-11-23 09:13:21 -0500

    Finish date class

---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace POOConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("POO Concepts");
+            Console.WriteLine("============");
+            
+            Date date1 = new Date(2022, 2, 11);
+            Console.WriteLine(date1);
+
+            try
+            {
+                Console.WriteLine(new Date(2022, 2, 29));
+                Console.WriteLine(new Date(1974, 9, 23));
+                Console.WriteLine(new Date(1985, 11, 45));
+            }
+            catch (Exception error)
+            {
+
+                Console.WriteLine(error.Message);
+            }
+
+        }
+    }
+}
